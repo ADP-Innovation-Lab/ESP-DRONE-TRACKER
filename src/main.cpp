9b8dffaecadf494e90e9c0228b706a79196b591a
Mr-JoE1 <mrjoe767@gmail.com>
2024-05-04 23:48:25 +0400

    freeRTOS added

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,66 @@
 #include <ArduinoJson.h>
 #include "myGPS.h"
 #include "myIMU.h"
+
 myGPS witGPS;
+
+//------------ freeRTOS defs
+TaskHandle_t AppHandlde = NULL;
+TaskHandle_t sensorsHandle = NULL;
 
 //------------- varaibles and defs
 uint8_t rtAttemp = 5;
-
 DynamicJsonDocument doc(MQTT_BUFFER_ZISE);
-
 unsigned long lastPublishTime = 0;
 const unsigned long publishInterval = 10000; // Publish payload every 5 seconds
+
 //-------------- functions
 String create_jsonPayload();
+void appTask(void *pvParameters);
+void sensorsTask(void *pvParameters);
 
 void setup()
 {
   Serial.begin(115200);
+
+  // create app task
+  xTaskCreatePinnedToCore(
+      appTask,     // Task function
+      "appTask",   // Task name
+      10000,       // Stack size (bytes)
+      NULL,        // Task parameters
+      1,           // Priority (1 is highest priority)
+      &AppHandlde, // Task handle
+      0            // Core to run the task on (0 or 1)
+  );
+
+  // create app task
+  xTaskCreatePinnedToCore(
+      sensorsTask,    // Task function
+      "sensorsTask",  // Task name
+      10000,          // Stack size (bytes)
+      NULL,           // Task parameters
+      1,              // Priority (1 is highest priority)
+      &sensorsHandle, // Task handle
+      1               // Core to run the task on (0 or 1)
+  );
+}
+
+void loop()
+{
+}
+
+/**
+ * Main App thread , includes LTE, Mqtt and GPS
+ *
+ * @param pvParameters
+ */
+
+void appTask(void *pvParameters)
+{
+  DBG("[APP] App thead init ...");
   lte_setup();
   witGPS.setup();
-  imu_setup();
 
   while (!lte_connect() && rtAttemp > 0)
   {
@@ -30,62 +72,87 @@ void setup()
   }
 
   lte_mqttSetup();
-}
 
-void loop()
+  while (1)
+  {
+    witGPS.loop();
+    witGPS.readablePrintEx();
+
+    // Periodic payload publish
+    unsigned long currentMillis = millis();
+    if (currentMillis - lastPublishTime >= publishInterval)
+    {
+      lastPublishTime = currentMillis;
+      if (lte_isMqttConnected())
+      {
+        lte_mqttPublish(create_jsonPayload());
+        DBG("[APP] MQTT Payload published ");
+      }
+      else
+      {
+        lte_mqttReconnect();
+        DBG("[APP] MQTT Payload NOT published ");
+      }
+    }
+    lte_mqttLoop();
+    delay(500);
+  }
+}
+/**
+ * @brief Sensors fusion thread, includes IMU, BME, Anti-tampring and Battery
+ *
+ * @param pvParameters
+ */
+void sensorsTask(void *pvParameters)
 {
 
-  witGPS.loop();
-  witGPS.readablePrintEx();
-
-  imu_loop();
-  imu_print();
-
-  // Periodic payload publish
-  unsigned long currentMillis = millis();
-  if (currentMillis - lastPublishTime >= publishInterval)
+  unsigned long preriodiMills = 0;
+  DBG("[APP] Sensors thead init ...");
+  imu_setup();
+  while (1)
   {
-    lastPublishTime = currentMillis;
-    if (lte_isMqttConnected())
+    imu_loop();
+    // Periodic stuff
+    if (millis() - preriodiMills >= 1000)
     {
-      lte_mqttPublish(create_jsonPayload());
-      DBG("[APP] MQTT Payload published ");
+      preriodiMills = millis();
+      imu_print();
     }
-    else
-    {
-      lte_mqttReconnect();
-      DBG("[APP] MQTT Payload NOT published ");
-    }
+
+    delay(50);
   }
-  lte_mqttLoop();
-  delay(500);
 }
 
+/**
+ * @brief Create a jsonPayload object
+ *
+ * @return String
+ */
 String create_jsonPayload()
 {
 
-  doc["id"] = "DT101";                     // Use device IMEI as ID
-  doc["timestamp"]  = witGPS.getDateTimeStr(); // modem.getGSMDateTime(DATE_FULL); // Get modem time for timestamp
-  doc["latitude"]   = witGPS.getLatitude();
-  doc["longitude"]  = witGPS.getLongitude();
+  doc["id"] = "DT101";                        // Use device IMEI as ID
+  doc["timestamp"] = witGPS.getDateTimeStr(); // modem.getGSMDateTime(DATE_FULL); // Get modem time for timestamp
+  doc["latitude"] = witGPS.getLatitude();
+  doc["longitude"] = witGPS.getLongitude();
 
   JsonObject deviceData = doc["deviceData"].to<JsonObject>();
   deviceData["status"] = "Flying";
-  deviceData["event"]  = "DataUpdate";
+  deviceData["event"] = "DataUpdate";
 
   JsonObject battery = deviceData["battery"].to<JsonObject>();
   battery["voltage"] = 3.7;
   battery["percentage"] = 80;
 
   JsonObject location = deviceData["location"].to<JsonObject>();
-  location["latitude"]  = witGPS.getLatitude();
+  location["latitude"] = witGPS.getLatitude();
   location["longitude"] = witGPS.getLongitude();
-  location["altitude"]  = witGPS.getAltitude();
+  location["altitude"] = witGPS.getAltitude();
 
   JsonObject imu = deviceData["imu"].to<JsonObject>();
-  imu["roll"]  = imu_getRoll(); 
+  imu["roll"] = imu_getRoll();
   imu["pitch"] = imu_getPitch();
-  imu["yaw"]   = imu_getYaw();
+  imu["yaw"] = imu_getYaw();
   doc["lastFlightStart"] = "";
   doc["lastFlightStop"] = "";
   doc["speed"] = witGPS.getSpeed();
